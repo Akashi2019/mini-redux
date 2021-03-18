acdfbdc4477d511c74b0fdc08ac6a71ea76569a5
Akashi2019 <657318304@qq.com>
2021-03-18 11:32:09 +0800

    feat: compose

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import './App.css';
 import ContextPage from './pages/ContextPage';
 
+function f1(arg) {
+  return arg;
+}
+
+function f2(arg) {
+  return arg;
+}
+
+function f3(arg) {
+  return arg;
+}
+
+const res = compose(f1, f2, f3)('compose');
+console.log('res: ', res);
+
+function compose(...fns) {
+  if (fns.length === 0) {
+    return (arg) => arg;
+  }
+  if (fns.length === 1) {
+    return fns[0];
+  }
+  return fns.reduce((a, b) => (...args) => a(b(args)));
+}
+
 function App() {
   return (
     <div className="App">
